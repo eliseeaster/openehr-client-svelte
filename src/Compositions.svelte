888682898fd7d810ab5d61a389d4a6e983bb8849
Elise Easter Christiansen <eec@dips.no>
2022-03-28 12:21:51 +0200

    trying to make node work

--- src/Compositions.svelte
+++ src/Compositions.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { openEHR } from './openehrStore.ts';        
-    import CompositionFactory from './compositionFactory.ts';
+  import { openEHR } from "./openehrStore.ts";
+  import CompositionFactory from "./compositionFactory.ts";
 
-    const aql = `
+  const aql = `
         select 
             c/context/start_time/value as start_time,
             c/name/value as name
@@ -11,44 +11,42 @@
         limit $count
     `;
 
-    var queryParams = {};
-    queryParams.count = 25;
+  var queryParams = {};
+  queryParams.count = 25;
 
-    function postComposition()
-    {
-        $openEHR.compose(new CompositionFactory().getComposition());
-    }
+  //   function postComposition() {
+  //     $openEHR.compose(new CompositionFactory().getComposition());
+  //   }
 
+  function postComposition() {
+    $openEHR.compose(new CompositionFactory().getComposition(), -1502, 1002160);
+  }
 </script>
-<div>
-    <h1>
-        Last 25 compositions
-    </h1>
-    <div>
-        <button on:click="{postComposition}">
-            Post Composition
-        </button>        
-    </div>
-    <div>
-        {#await $openEHR.query(aql, queryParams)}
-            Loading...
-        {:then result}
-        <table>
-            <thead>
-                <tr>
-                    <th>Start time</th>
-                    <th>Name</th>
-                </tr>
-            </thead>
-            {#each result.rows as row}
-            <tr>
-                <td>{row.items[0]}</td><td>{row.items[1]}</td>
-            </tr>
-            {/each}
-        </table>    
-        {:catch error}
-            Failed to load compositions {error}
-        {/await}
-    </div>    
-</div>
 
+<div>
+  <h1>Last 25 compositions</h1>
+  <div>
+    <button on:click={postComposition}> Post Composition </button>
+  </div>
+  <div>
+    {#await $openEHR.query(aql, queryParams)}
+      Loading...
+    {:then result}
+      <table>
+        <thead>
+          <tr>
+            <th>Start time</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        {#each result.rows as row}
+          <tr>
+            <td>{row.items[0]}</td><td>{row.items[1]}</td>
+          </tr>
+        {/each}
+      </table>
+    {:catch error}
+      Failed to load compositions {error}
+    {/await}
+  </div>
+</div>
